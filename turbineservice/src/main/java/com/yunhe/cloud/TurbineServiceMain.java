d55075accddaee0d889b1068eef564a0ede80984
zhangwei@yunheit.com <zhangwei523>
2019-06-12 18:45:18 +0800

    修改turbine

--- turbineservice/src/main/java/com/yunhe/cloud/TurbineServiceMain.java
+++ turbineservice/src/main/java/com/yunhe/cloud/TurbineServiceMain.java
@@ -1,16 +1,30 @@
 package com.yunhe.cloud;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableTurbine
+@EnableHystrixDashboard
 @EnableDiscoveryClient
 @EnableEurekaClient
 public class TurbineServiceMain {
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
     public static void main(String[] args) {
         SpringApplication.run(TurbineServiceMain.class,args);
     }
