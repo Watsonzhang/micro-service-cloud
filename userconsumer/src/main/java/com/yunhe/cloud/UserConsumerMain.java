00b652803873d7b98b0ca459c61a1f2301e80a2e
zhangwei@yunheit.com <zhangwei523>
2019-06-12 14:19:20 +0800

    添加hystrix dashboard

--- userconsumer/src/main/java/com/yunhe/cloud/UserConsumerMain.java
+++ userconsumer/src/main/java/com/yunhe/cloud/UserConsumerMain.java
@@ -1,10 +1,14 @@
 package com.yunhe.cloud;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -18,9 +22,21 @@ import org.springframework.web.client.RestTemplate;
  */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableHystrixDashboard
 public class UserConsumerMain {
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
+
     @Bean
     @LoadBalanced
     RestTemplate restTemplate(){
