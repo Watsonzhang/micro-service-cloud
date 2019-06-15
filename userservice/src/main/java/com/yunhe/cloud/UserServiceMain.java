73902e32593da70bacb799c7713e7f8ed9c05dea
zhangwei <664886892@qq.com>
2019-06-15 17:49:07 +0800

    先去除spring config

--- userservice/src/main/java/com/yunhe/cloud/UserServiceMain.java
+++ userservice/src/main/java/com/yunhe/cloud/UserServiceMain.java
@@ -2,6 +2,7 @@ package com.yunhe.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -13,6 +14,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 public class UserServiceMain {
     public static void main(String[] args) {
         SpringApplication.run(UserServiceMain.class,args);
