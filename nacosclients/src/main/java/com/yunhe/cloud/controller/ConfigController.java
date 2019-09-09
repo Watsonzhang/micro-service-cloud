5806e81c8810ac4c2843fe9b078c34bf2c62092e
zhangwei@yunheit.com <zhangwei@yunheit.com>
2019-09-09 18:24:03 +0800

    测试nacos

---
+++ nacosclients/src/main/java/com/yunhe/cloud/controller/ConfigController.java
@@ -0,0 +1,21 @@
+package com.yunhe.cloud.controller;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/config")
+@RefreshScope
+public class ConfigController {
+
+    @Value("${useLocalCache:false}")
+    private boolean useLocalCache;
+
+    @RequestMapping("/get")
+    public boolean get() {
+        return useLocalCache;
+    }
+}
