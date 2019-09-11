6b5d44cbcac59693eec6636e05bcdac4afdf7bda
zhangwei@yunheit.com <zhangwei@yunheit.com>
2019-09-11 17:44:28 +0800

    修改服务控制器 和网关配置

--- userconsumer/src/main/java/com/yunhe/cloud/controller/ConsumerController.java
+++ userconsumer/src/main/java/com/yunhe/cloud/controller/ConsumerController.java
@@ -7,11 +7,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 public class ConsumerController {
 
     @Autowired
     private ConsumerServiceFeign consumerServiceFeign;
+
+    @GetMapping
+    public String sayHello(HttpServletRequest request){
+        System.out.println(request.getRequestURL());
+       return request.getRequestURL().toString();
+    }
     @GetMapping("/consumer/{id}")
     public Consumer getConsumerById(@PathVariable Integer id){
        return consumerServiceFeign.getById(id);
